6da071208f693b1a8a9918dd99ddd4f8081c6efa
QuentinGeerts <quentin.geerts@bstorm.be>
2025-01-13 15:06:27 +0100

    Demo - Delegate 01 (NotificationService)

---
+++ Demo-Delegate1/Models/NotificationService.cs
@@ -0,0 +1,33 @@
+﻿namespace Demo_Delegate1.Models;
+
+// Définir le délégué pour les méthodes de notification
+delegate void NotifyDelegate (string message);
+
+internal class NotificationService
+{
+    // Déclaration de la variable de type du délégué
+    private NotifyDelegate _notifyDelegate;
+
+    // Méthode pour enregistrer la / les méthode(s) de notification
+    public void RegisterNotification(NotifyDelegate notifyDelegate)
+    {
+        _notifyDelegate += notifyDelegate;
+    }
+
+    // Méthode pour désenregistrer la / les méthode(s) de notification
+    public void UnRegisterNotification(NotifyDelegate notifyDelegate)
+    {
+        if (_notifyDelegate != null)
+            _notifyDelegate -= notifyDelegate;
+    }
+
+    // Méthode pour envoyer la notification
+    public void Notify(string message)
+    {
+        _notifyDelegate?.Invoke(message);
+
+        // Autre façon 
+
+        // if (_notifyDelegate != null) _notifyDelegate(message);
+    }
+}
---
+++ Demo-Delegate1/Program.cs
@@ -0,0 +1,33 @@
+﻿using Demo_Delegate1.Models;
+
+NotificationService notificationService = new NotificationService();
+
+// Enregistrer les méthodes de notification
+notificationService.RegisterNotification(SendEmail);
+notificationService.RegisterNotification(SendPigeon);
+notificationService.RegisterNotification(SendSMS);
+notificationService.RegisterNotification(SendEmail);
+
+
+notificationService.UnRegisterNotification(SendEmail);
+notificationService.UnRegisterNotification(SendEmail);
+notificationService.UnRegisterNotification(SendEmail);
+
+// Envoyer la notification
+notificationService.Notify("Hello World");
+
+
+static void SendEmail(string message)
+{
+    Console.WriteLine($"Envoi d'email: {message}");
+}
+
+static void SendSMS(string message)
+{
+    Console.WriteLine($"Envoi SMS: {message}");
+}
+
+static void SendPigeon(string message)
+{
+    Console.WriteLine($"Envoi Pigeon: {message}");
+}
